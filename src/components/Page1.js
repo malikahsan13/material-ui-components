b8513ea7b0e16a318f832b79112e8e18495edd64
MalikAhsan-PC <malikahsan13@gmail.com>
2022-12-26 23:16:49 +0500

    added container component

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -17,8 +17,9 @@ import {
   DialogActions,
   Modal,
   Box,
-  Link
+  Link,
 } from "@mui/material";
+import { Container } from "@mui/system";
 import React, { useState } from "react";
 
 function Page1() {
@@ -45,53 +46,71 @@ function Page1() {
           </Button>
         </Toolbar>
       </AppBar>
-      <Card position="absolute" sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            sx={{ height: 140 }}
-            image="https://i.natgeofe.com/n/5506954d-6e0f-444c-aa83-a24adbe874e1/reptiles-hero_4x3.jpg"
-            title="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small" onClick={()=>setOpenDialog(true)}>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Dialog open={openDialog} onClose={()=>setOpenDialog(false)}>
-        <DialogTitle>
-            Are you sure ?
-        </DialogTitle>
-        <DialogContent>
+      <Container maxWidth="sm">
+        <Card position="absolute" sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://i.natgeofe.com/n/5506954d-6e0f-444c-aa83-a24adbe874e1/reptiles-hero_4x3.jpg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small" onClick={() => setOpenDialog(true)}>
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+        <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+          <DialogTitle>Are you sure ?</DialogTitle>
+          <DialogContent>
             <DialogContentText>
-                This Form will be submitted if you agree.
+              This Form will be submitted if you agree.
             </DialogContentText>
-        </DialogContent>
-        <DialogActions>
+          </DialogContent>
+          <DialogActions>
             <Button variant="contained">Cancel</Button>
             <Button variant="contained">Agree</Button>
-        </DialogActions>
-      </Dialog>
-      <Button onClick={()=>{setModalOpen(true)}} variant="contained">Open Modal</Button>
-      <Modal open={modalOpen} onClose={()=>setModalOpen(false)} sx={{color:"hideBackdrop"}}> 
-        <Box position="relative" top="50%" left="50%">
-            <Typography>
-                This is Modal Box
-            </Typography>
+          </DialogActions>
+        </Dialog>
+        <Button
+          onClick={() => {
+            setModalOpen(true);
+          }}
+          variant="contained"
+        >
+          Open Modal
+        </Button>
+        <Modal
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          sx={{ color: "hideBackdrop" }}
+        >
+          <Box position="relative" top="50%" left="50%">
+            <Typography>This is Modal Box</Typography>
             <Button variant="outlined">Ok</Button>
             <Button variant="outlined">Cancel</Button>
-        </Box>
-      </Modal>
-      <Link color="green" variant="h4" underline="hover" href="https://www.google.com">Visit Google</Link>
+          </Box>
+        </Modal>
+        <Link
+          color="green"
+          variant="h4"
+          underline="hover"
+          href="https://www.google.com"
+        >
+          Visit Google
+        </Link>
+      </Container>
     </div>
   );
 }
